9c72d8ad987bfdeffff5e3cf9fd48406c3bb14c9
ngnvhnm <namnam24042003@gmail.com>
2024-11-22 15:58:22 +0100

    - noch einmal

--- backend/src/main/java/com/dach/reservation_tool/general/GroupResponse.java
+++ backend/src/main/java/com/dach/reservation_tool/general/GroupResponse.java
@@ -1,2 +1,18 @@
-package com.dach.reservation_tool.general;public class GroupResponse {
+package com.dach.reservation_tool.general;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class GroupResponse {
+
+    private String group;
+    private List<String> items;
+
+    public GroupResponse(String group, List<String> items) {
+        this.group = group;
+        this.items = items;
+    }
 }
