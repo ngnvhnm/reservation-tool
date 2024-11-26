1352d7a861ef56aff4680ecdf38e09d68c19b166
VS <v.s@dachpc.com>
2024-11-26 18:07:42 +0100

    Implement logout

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,36 +1,17 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  IconChevronDown,
-  IconHeart,
   IconLogout,
-  IconMessage,
-  IconPlayerPause,
-  IconSettings,
-  IconStar,
-  IconSwitchHorizontal,
-  IconTrash,
 } from '@tabler/icons-react';
-import cx from 'clsx';
 import {
-  Avatar,
-  Burger,
+  Button,
   Container,
   Group, Image,
-  Menu,
   Tabs,
-  Text,
-  UnstyledButton,
   useMantineTheme,
 } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import classes from './Header.module.css';
-
-const user = {
-  name: 'Jane Spoonfighter',
-  email: 'janspoon@fighter.dev',
-  image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
-};
+import keycloak from '../../providers/authentication/keycloak.ts';
 
 const tabs = [
   'booking',
@@ -40,9 +21,6 @@ const tabs = [
 
 export function Header() {
   const theme = useMantineTheme();
-  const [opened, { toggle }] = useDisclosure(false);
-  const [userMenuOpened, setUserMenuOpened] = useState(false);
-
   const items = tabs.map((tab) => (
     <Tabs.Tab value={tab} key={tab}>
       {tab.charAt(0).toUpperCase() + tab.slice(1)}
@@ -62,67 +40,14 @@ export function Header() {
       <Container className={classes.mainSection} size="md">
         <Group justify="space-between">
           <Image src={"https://www.dachpc.com/wp-content/uploads/2024/04/DACHPC_Logo_19_04_2024_color_flach.svg"}  />
-
-          <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
-
-          <Menu
-            width={260}
-            position="bottom-end"
-            transitionProps={{ transition: 'pop-top-right' }}
-            onClose={() => setUserMenuOpened(false)}
-            onOpen={() => setUserMenuOpened(true)}
-            withinPortal
-          >
-            <Menu.Target>
-              <UnstyledButton
-                className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
-              >
-                <Group gap={7}>
-                  <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
-                  <Text fw={500} size="sm" lh={1} mr={3}>
-                    {user.name}
-                  </Text>
-                  <IconChevronDown size={12} stroke={1.5} />
-                </Group>
-              </UnstyledButton>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item
-                leftSection={<IconHeart size={16} color={theme.colors.red[6]} stroke={1.5} />}
-              >
-                Liked posts
-              </Menu.Item>
-              <Menu.Item
-                leftSection={<IconStar size={16} color={theme.colors.yellow[6]} stroke={1.5} />}
-              >
-                Saved posts
-              </Menu.Item>
-              <Menu.Item
-                leftSection={<IconMessage size={16} color={theme.colors.blue[6]} stroke={1.5} />}
-              >
-                Your comments
-              </Menu.Item>
-
-              <Menu.Label>Settings</Menu.Label>
-              <Menu.Item leftSection={<IconSettings size={16} stroke={1.5} />}>
-                Account settings
-              </Menu.Item>
-              <Menu.Item leftSection={<IconSwitchHorizontal size={16} stroke={1.5} />}>
-                Change account
-              </Menu.Item>
-              <Menu.Item leftSection={<IconLogout size={16} stroke={1.5} />}>Logout</Menu.Item>
-
-              <Menu.Divider />
-
-              <Menu.Label>Danger zone</Menu.Label>
-              <Menu.Item leftSection={<IconPlayerPause size={16} stroke={1.5} />}>
-                Pause subscription
-              </Menu.Item>
-              <Menu.Item color="red" leftSection={<IconTrash size={16} stroke={1.5} />}>
-                Delete account
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+            <Button
+              color={theme.colors.blue[6]}
+              leftSection={<IconLogout size={16} stroke={1.5} />}
+              onClick={() => {
+                console.log('logout')
+                keycloak.logout()
+              }}
+          >Logout</Button>
         </Group>
       </Container>
       <Container size="md">
